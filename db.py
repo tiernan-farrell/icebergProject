2af0b3f44958499c92b0ded8dea5b2232416ca47
tiernan-farrell <farrell.331@osu.edu>
2022-09-13 14:52:22 -0400

    Scraper scrapes data on 24 rbs and stores data in players.db

--- db.py
+++ db.py
@@ -4,10 +4,40 @@ conn = sqlite3.connect('db/players.db')
 
 c = conn.cursor()
 
-c.execute("""CREATE TABLE players (
-            firstName text, 
-            lastName text 
+c.execute("""CREATE TABLE playerData (
+            player_week_id INTEGER PRIMARY KEY,
+            player_id INTEGER, 
+            first_name TEXT NOT NULL, 
+            last_name TEXT NOT NULL,
+            game_num INTEGER, 
+            game_date TEXT,
+            team TEXT,
+            opp TEXT,
+            game_result TEXT,
+            rush_att INTEGER, 
+            rush_yds INTEGER, 
+            rush_yds_per_att REAL, 
+            rush_td INTEGER, 
+            targets INTEGER,
+            rec INTEGER, 
+            rec_yds INTEGER,
+            rec_yds_per_rec REAL,
+            rec_td INTEGER,
+            catch_pct TEXT,
+            rec_yds_per_tgt REAL,
+            all_td INTEGER,
+            scoring INTEGER,
+            fumbles INTEGER,
+            fumbles_lost INTEGER,
+            fumbles_forced INTEGER,
+            fumbles_rec INTEGER,
+            fumble_rec_yards INTEGER,
+            fumble_rec_tds INTEGER,
+            offense INTEGER,
+            off_pct TEXT,
+            defense INTEGER,
+            def_ptc TEXT,
+            special_teams INTEGER,
+            st_pct TEXT
             )""")
-
-
 
--- scrape.py
+++ scrape.py
@@ -1,38 +1,18 @@
 import requests
-
+import sqlite3
 from bs4 import BeautifulSoup
 
 
-def get_stats():
-    response = requests.get('https://www.pro-football-reference.com/players/M/MoorD.00/gamelog/')
+# This file scraps data on rb stats from 2021 from the url below with different RB's. 
+# This data is added to the players.db sqlite3 db 
+
+URL = 'https://www.pro-football-reference.com/players/'
+YEAR = '/gamelog/2021/'
+
+def get_stats(player):
+    endPoint = URL + player + YEAR
+    response = requests.get(endPoint)
     return response.content
-
-
-def get_stat_for_season(html_game_stats, stat_to_scrape):
-    """
-    Function that extracts a given stat from PFR table rows 
-    and returns the results in a list
-    
-    :param html_game_stats: BeautifulSoup PageElements containing our stats
-    :param stat_to_scrape: PFR stat_id to extract
-    :return: List of stats, 16-weeks big, for player's season
-    """
-    scraped_weekly_stats = []
-    game_being_processed = 1
-    for game in html_game_stats:
-        cur_pfr_game = int(get_stat_value_from_game(game, "week_num"))
-
-        if player_has_missed_games(game_being_processed, cur_pfr_game):
-            scraped_weekly_stats = fill_in_missed_games(
-                game_being_processed, cur_pfr_game, scraped_weekly_stats
-            )
-
-        game_receptions = get_stat_value_from_game(game, stat_to_scrape)
-        scraped_weekly_stats.append(game_receptions)
-        game_being_processed += 1
-
-    check_if_missed_week_sixteen(scraped_weekly_stats)
-    return scraped_weekly_stats
 
 
 def get_stat_value_from_game(game, pfr_stat_id):
@@ -44,52 +24,54 @@ def get_stat_value_from_game(game, pfr_stat_id):
     :return: Extracted stat for provided game
     """
     data_stat_rec = game.find("td", {"data-stat": pfr_stat_id})
+    if data_stat_rec is None: 
+        return 0
     stat_val = data_stat_rec.renderContents().strip()
     return stat_val.decode("utf-8")
-    
-    
-def player_has_missed_games(game_being_processed, cur_pfr_game):
-    """
-    Function that checks if the player has missing games
-    :param game_being_processed: Game (by week) we are processing
-    :param cur_pfr_game: Game (by week) we are on for the player
-    :return: Bool
-    """
-    if game_being_processed == cur_pfr_game:
-        return False
 
-    return True
-
-def fill_in_missed_games(game_being_processed, cur_pfr_game, stat_per_game):
-    """
-    Function that fills a list with 0s for missing games
-    
-    :param game_being_processed: Game (by week) we are processing
-    :param cur_pfr_game: Game (by week) we are on for the player
-    :param stat_per_game: List containing a stat's value for each game
-    :return: Updated list with missing games filled in with "0"
-    """
-    games_missed = cur_pfr_game - game_being_processed
-    for i in range(games_missed):
-        stat_per_game.append("0")
-
-    return stat_per_game
-
-
-def check_if_missed_week_sixteen(stat_per_game):
-    """
-    Function that checks a list of stats per game is the
-    expected 16 weeks big
-
-    :param stat_per_game: List of stats
-    :return: Updated list, if applicable
-    """
-    if len(stat_per_game) != 16:
-        stat_per_game.append("0")
 
 if __name__ == "__main__": 
-    res = get_stats()
-    soup = BeautifulSoup(res, "html.parser")
-    game_stats = soup.find_all("tr", id=lambda x: x and x.startswith("stats."))
-    rec = get_stat_for_season(game_stats, "rec")
-    print(sum([int(n) for n in rec]))
+    conn = sqlite3.connect('db/players.db')
+    c = conn.cursor()
+    
+   # 25 of the highest ranked fantasy fb rbs that played in 2021
+    players = ['T/TaylJo02', 'E/EkelAu00', 'C/CookDa01', 'H/HenrDe00',
+               'H/HarrNa00', 'S/SwifDA00', 'M/MixoJo00', 'K/KamaAl00',
+               'B/BarkSa00', 'J/JoneAa00', 'F/FourLe00', 'W/WilllJa10',
+               'C/ChubNi00', 'C/ConnJa00', 'E/ElliEz00', 'E/EtieTr00',
+               'M/MontDa01', 'D/DillAJ00', 'A/AkerCa00', 'M/MitcEl00',
+               'E/EdmoCh00', 'J/JacoJo01', 'G/GibsAn00', 'H/HuntKa00', 'S/SandMi01']
+    
+    stats = ['game_num', 'game_date', 'team', 'opp', 'game_result', 'rush_att', 'rush_yds', 'rush_yds_per_att', 'rush_td', 'targets', 'rec', 'rec_yds', 'rec_yds_per_rec', 'rec_td', 'catch_pct', 'rec_yds_per_tgt', 'all_td', 'scoring', 'fumbles', 'fumbles_lost', 'fumbles_forced', 'fumbles_rec', 'fumbles_rec_yds', 'fumbles_rec_td', 'offense', 'off_pct', 'defense', 'def_pct', 'special_teams', 'st_pct']
+   
+    for idx, player in enumerate(players): 
+        player_data = requests.get(URL + player + '.htm')
+        s = BeautifulSoup(player_data.content, "html.parser")
+        title = str(s.title)
+        split = title.split(' ')
+        first_name = str(split[0]).split('>')[1]
+        last_name = split[1]
+        res = get_stats(player)
+        soup = BeautifulSoup(res, "html.parser")
+        game_stats = soup.find_all("tr", id=lambda x: x and x.startswith("stats."))
+        for game in game_stats:
+            cur_pfr_game = int(get_stat_value_from_game(game, "week_num"))
+            id = int(str(idx) + str(cur_pfr_game))
+            print(id)
+            data = {"player_id": idx, "first_name": first_name, "last_name": last_name}
+            for stat in stats: 
+                val = get_stat_value_from_game(game, stat)
+                if stat == 'game_date' or stat == 'team' or stat == 'opp' or stat == 'game_result':
+                    val = str(str(val).split('>')[1]).split('<')[0]
+                data[stat] = val
+                
+            sql = "INSERT INTO playerData(player_id, first_name, last_name, game_num, game_date, team, opp, game_result, rush_att, rush_yds, rush_yds_per_att, rush_td, targets, rec, rec_yds, rec_yds_per_rec, rec_td, catch_pct, rec_yds_per_tgt, all_td, scoring, fumbles, fumbles_lost, fumbles_forced, fumbles_rec, fumble_rec_yards, fumble_rec_tds, offense, off_pct, defense, def_ptc, special_teams, st_pct) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+            try: 
+                c.execute(sql, tuple(data.values()))
+                conn.commit()
+            except sqlite3.Error as error:
+                print("Failed to insert multiple records into sqlite table", error)
+            
+            
+            
+    
