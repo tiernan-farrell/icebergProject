66ed49db62f223ad6ff71e01c289e1c9373504c1
tiernan-farrell <farrell.331@osu.edu>
2022-10-21 15:58:28 -0400

    BUC implementation done

--- workingBucMaybe.py
+++ workingBucMaybe.py
@@ -2,8 +2,8 @@
 from numpy import partition
 
 
-NUM_DIMS = 5
-CARDINALITY = [7 for i in range(NUM_DIMS)]
+NUM_DIMS = 3
+CARDINALITY = [3 for i in range(NUM_DIMS)]
 MINSUP = 2
 
 dataCount = []
@@ -27,21 +27,56 @@ def bucPartition(input: list, d: int, C: int):
 
     
 def buc(input: list, dim: int, prettyPrintLocation: list):
+    # print('buc call made Params: ', input, ', ', dim)
+    # print('\n', sorted(input, key=lambda a: a[dim]), '\n')
+    # Here 5 is the max number of attributes
+    # print('Region: ', len(input)) 
     print(prettyPrintLocation, ': ', len(input))
     for i in range(dim, NUM_DIMS):
         C = CARDINALITY[i]
+        # print('i: ', i)
+        # first sort by attribute index
         table = sortTupleList(input, i)
         bucPartition(input, i, C)
+        # print('table: ', table)
+        # print('dc: ', dataCount)
+        # print('\n 
+        # Table for input: ', input, '\n dim: ', dim, '\n Table: ', table)
+        # Then scan the new list and check the attribute index 
+        # oldVal = (0, table[0][dim])
+        # print(oldVal, '\n\n')
         k = 0
         for j in range(C): 
+            # Get the current val
+            # print(j)
             c = dataCount[i][j]
             if (c >= MINSUP):
+                # print('count of value: ', j, ' in dim: ', i, ' = ', c)
+                
+                # if(k < len(table)):
+                    # print(table[k], ' ', c)
+                # if(i+1 < NUM_DIMS and k+c < len(table)):
                 newLocation = prettyPrintLocation
                 newLocation[i] = j
                 buc([table[idx] for idx in range(k, k+c)], i+1, newLocation)
             k+= c
         prettyPrintLocation[i] = '*'
     return
+        # print('\n BIG ITERATION DONE - dim: ', i)
+            # newVal = (j, table[j][dim])
+            # Check if it is a new val 
+            # if (newVal[1] != oldVal[1]):
+            #     # print('\n miss: ', newVal[1], ' ', oldVal[1], '\n')
+            #     # we also check our count to see if it is above the minsup 
+            #     if (newVal[0]-oldVal[0] >= MINSUP): 
+            #         if (i+1 < 5):
+            #             newState = state
+            #             newState[i] = oldVal[1]
+            #             buc([table[idx] for idx in range(oldVal[0], j)], i+1, newState)
+                        
+            #     # When we reach a different value we update oldval 
+            #     # print('miss')
+            #     oldVal= (j, newVal)
         
         
 def processData(filename): 
