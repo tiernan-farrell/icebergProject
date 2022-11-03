a261bde0241a73069e1cdd5f3178b58349daddec
tiernan-farrell <63930496+tiernan-farrell@users.noreply.github.com>
2022-11-03 14:47:09 -0400

    Modified tdc to work properly (#14)
    Co-authored-by: tiernan-farrell <farrell.331@osu.edu>

--- tdc.py
+++ tdc.py
@@ -5,18 +5,11 @@ class TDC:
         self.minsup = minsup 
         self.dc = [[0 for _ in range(card[i])]for i in range(dims)]
         self.outList = []
+        self.checkedDims = []
         self.cube = {}
     #Sorts a list of tuples based on the ith index of value
     def sortTupleList(self, input: list, i: int): 
         return sorted(input, key=lambda a: a[i])
-
-    #Takes sorted input on dim d and puts the counts of instances of each 
-    #value in its corresponding spot in the datacount array 
-    # "datacount contains the number of records for each distinct value of the d-th dimension."
-    def bucPartition(self, input: list, d: int, C: int): 
-        self.dc[d] = [0 for i in range(C)]
-        for i in range(len(input)):
-            self.dc[d][input[i][d]] += 1
 
     def starKey(self, row: tuple, dims: list): 
         elements = []
@@ -33,36 +26,50 @@ class TDC:
         trimList.reverse()
 
         if l == trimList:
+            print('list to be trimmed: ', l)
             return True
         
         return False
 
     def trim(self, lis: list): 
-        newList = [i for i in range(len(lis)-2)]
-        newList.append(self.dims-1)
+        if len(lis) == 2: 
+            newList = lis
+            newList.pop()
+        else: 
+            newList = [i for i in range(len(lis)-1)]
+
         return newList
         
     def inc(self, lis: list): 
+        if len(lis) == 1: 
+            if (lis[0] < self.dims-1):
+                return [lis[0] + 1]
         newList = lis
-        check = len(lis)-2
         end = len(lis) - 1
         flag = True
-        while flag and len(lis) > 1 and check >= 0:
-            if lis[check] < lis[end]-1:
-                newList[check] = newList[check] + 1
+        while flag and len(lis) > 1 and end >= 0:
+            if lis[end] < self.dims - 1 - (len(lis)-1 - end):
+                newList[end] = lis[end] + 1
+                if end == 0:
+                    return [i for i in range(newList[end], len(lis)+newList[end])]
                 flag = False
             else: 
-                check -= 1
                 end -= 1
         return newList
-        
-        
+    
+    
+    
     def tdc(self, input: list, dimensionList: list):
+        for i in range(len(dimensionList)):
+            if [dimensionList[j] for j in range(i+1)] not in self.checkedDims:
+                self.checkedDims.append([dimensionList[j] for j in range(i+1)])
+        print(self.checkedDims)
         sortedInput = sorted(input, key=lambda x: tuple([x[dimensionList[i]]] for i in range(len(dimensionList))))
         d = {}
         prune = {}
         # Loop through input sorted on dimension order and insert counts into cube
         for row in sortedInput: 
+            
             for i in range(len(dimensionList)):
                 key = [dimensionList[j] for j in range(i+1)]
                 # print('Row: ', row, '   key: ', key, '  ')
@@ -70,26 +77,35 @@ class TDC:
                 starredKey = self.starKey(row, key)
                 if starredKey in d:
                     d[starredKey] += 1 
+                    # prune[starredKey].append(row) 
                 else: 
                     d[starredKey] = 1 
+                    # prune[starredKey] = []
+                    # prune[starredKey].append(row) 
         for key in d: 
             if key not in self.cube:
                 if d[key] >= self.minsup:
                     self.cube[key] = d[key]
-                else: 
-                    #prune input 
-                    pass 
+                # else: 
+                    # #prune input 
+                    # for row in prune[key]:
+                    #     input = input.delete(row)
         # Recursive calls happen dimension list is not empty 
-        if len(dimensionList) > 1:
+        f = True
+        while len(dimensionList) > 0 and f:
+            print(dimensionList)
             # if dimension list needs to be trimmed
             if self.needsTrimmed(dimensionList):
                 dimensionList = self.trim(dimensionList)
-                print(dimensionList)
             else: 
                 dimensionList = self.inc(dimensionList)
+            if dimensionList not in self.checkedDims: 
+                f = False 
+                print('Recursive call: ', dimensionList)
+                self.tdc(input, dimensionList)            
+                    
             
-            self.tdc(input, dimensionList)            
-        else: 
+        if len(dimensionList) == 0: 
             self.cube[tuple(['*' for i in range(self.dims)])] = len(input)
             
         return  
@@ -97,5 +113,3 @@ class TDC:
     
     def getResults(self):
         return [ str(str(key) + ': ' + str(value)) for (key,value) in self.cube.items() if value >= self.minsup ]
-    
-    
