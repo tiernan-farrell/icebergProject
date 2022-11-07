01d184fe1ee4c2989e67b26e9ff12542f7fa2d9b
tiernan-farrell <63930496+tiernan-farrell@users.noreply.github.com>
2022-11-06 22:38:29 -0500

    added Minsup user input (#18)
    Co-authored-by: tiernan-farrell <farrell.331@osu.edu>

--- react-frontend/src/Components/Form.js
+++ react-frontend/src/Components/Form.js
@@ -19,7 +19,10 @@ const Form = ({onSubmit}) => {
                 <input type="text" id="dims" placeholder='Enter Number of Dimensions' onChange={(e) => setNumDims(e.target.value)}></input>
                 <label for="tuples">Enter Cardinality</label>
                 <input type="text" id="card" placeholder='Enter Cardinality' onChange={(e) => setCard(e.target.value)}></input>
-                <input type="submit" id="submit-button" onClick={onSubmit(numTuples, numDims, card)}></input>
+                <label for="minsup">Enter Minsup</label>
+                <input type="text" id="minsup" placeholder='Enter Minsup' onChange={(e) => setMinsup(e.target.value)}></input>
+                <input type="submit" id="submit-button" onClick={onSubmit(numTuples, numDims, card, minsup)}></input>
+    
             </form>
         </div>
         </>
