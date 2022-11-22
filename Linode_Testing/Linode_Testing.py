7f1fa57c447290948d8339adf3defa6271d61a07
tiernan-farrell <farrell.331@osu.edu>
2022-11-21 20:06:31 -0500

    Updated testing script

--- Linode_Testing/Linode_Testing.py
+++ Linode_Testing/Linode_Testing.py
@@ -1,7 +1,7 @@
-from Linode_Apriori import Apriori
-from Linode_BUC import BUC
-from Linode_Star_Cube import starCube
-from Linode_TDC import TDC
+from Algorithms.apriori import Apriori
+from Algorithms.buc import BUC
+from Algorithms.starCube import starCube
+from Algorithms.tdc import TDC
 import time
 import tracemalloc
 import psutil
@@ -45,96 +45,73 @@ algo_Input = ' '
 load_tuple=psutil.getloadavg()
 
 
-while(not algo_Input == '5'):
-    print('\nPlease enter the cardinality of the file.\n')
-    card = input()
-    print('\nPlease enter the number of dimensions of the file\n')
-    numDims = input()
-    print('\nPlease enter the minsup\n')
-    minsup = input()
-    print('\nPlease enter which algorithm you would like to run.')
-    algo_Input = input('1: Apriori 2: BUC 3: Star-Cube 4: TDC 5: to quit\n')
+def testDataSet(fileName: str, card: int, numDims):
+    data = processData('data/data.txt')
+    minsup =1 
+    while minsup <= 1000:
+        #Apriori
+        aprioriStart = time.perf_counter()
+        tracemalloc.start()
+        apriori = Apriori([card for i in range(numDims)], numDims, minsup)
+        apriori.apriori(data)
+        aprioriEnd = time.perf_counter()
+        aprioriTime = aprioriEnd - aprioriStart
+        apriori_Results = apriori.getResults()
+        f = open('results/Apriori_Results.txt', 'w')
+        f.write('Apriori time: ' + aprioriTime + '\n')
+        f.write('Apriori memory usage: '+ tracemalloc.get_traced_memory()+ '\n')
+        f.write('The average CPU usage is: '+ convert_to_percent(load_tuple))
+        f.write('List: \n' + apriori_Results)
+        print('*List printed to file Apriori_Results.txt*')
+        tracemalloc.stop()
+
+
+        # BUC
+        bucStart = time.perf_counter()
+        tracemalloc.start()
+        buc = BUC([card for i in range(numDims)], numDims, minsup)
+        buc.buc(data, 0, ['*' for i in range(numDims)])
+        bucEnd = time.perf_counter()
+        bucTime = bucEnd - bucStart
+        buc_Results = buc.getResults()
+        f = open('results/BUC_Results.txt', 'w')
+        f.write('BUC time: ' + bucTime + '\n')
+        f.write('BUC memory usage: '+ tracemalloc.get_traced_memory()+ '\n')
+        f.write('The average CPU usage is: '+ convert_to_percent(load_tuple))
+        f.write('List: \n' + buc_Results)
+        print('*List printed to file BUC_Results.txt*')
+        tracemalloc.stop()
     
-
-    match algo_Input:
-        case '1': #Apriori
-            data = processData('data/data.txt') 
-            start = time.perf_counter()
-            tracemalloc.start()
-            apriori = Apriori([card for i in range(numDims)], numDims, minsup)
-            apriori.apriori(data)
-            end = time.perf_counter()
-            time = end - start
-            toContinue = input('Algorithm finshed output list to txt? y/n: ')
-            if(toContinue == 'y'):
-                apriori_Results = apriori.getResults()
-                f = open('results/Apriori_Results.txt', 'w')
-                f.write('Apriori time: ' + time + '\n')
-                f.write('Apriori memory usage: '+ tracemalloc.get_traced_memory()+ '\n')
-                f.write('The average CPU usage is: '+ convert_to_percent(load_tuple))
-                f.write('List: \n' + apriori_Results)
-                print('*List printed to file Apriori_Results.txt*')
-            tracemalloc.stop()
+        #TDC
+        tdcStart = time.perf_counter()
+        tracemalloc.start()
+        tdc = TDC([card for i in range(numDims)], numDims, minsup)
+        tdc.tdc(data, [i for i in range(numDims)])
+        tdcEnd = time.perf_counter()
+        tdcTime = tdcEnd - tdcStart
+        tdc_results = tdc.getResults()
+        f = open('results/TDC_Results.txt', 'w')
+        f.write('TDC time: ' + tdcTime + '\n')
+        f.write('TDC memory usage: '+ tracemalloc.get_traced_memory()+ '\n')
+        f.write('The average CPU usage is: '+ convert_to_percent(load_tuple))
+        f.write('List: \n' + tdc_results)
+        print('*List printed to file TDC_Results.txt*')
+            
+        # Star Cubing    
+        scStart = time.perf_counter()
+        tracemalloc.start()
+        star = starCube(data, minsup)
+        star.generateCube()
+        scEnd = time.perf_counter()
+        scTime = scEnd - scStart
+        starCube_results = star.getResults()
+        f = open('results/StarCube_Results.txt', 'w')
+        f.write('Star-Cube time: ' + scTime + '\n')
+        f.write('Star-Cube memory usage: '+ tracemalloc.get_traced_memory()+ '\n')
+        f.write('The average CPU usage is: '+ convert_to_percent(load_tuple))
+        f.write('List: \n' + starCube_results)
+        print('*List printed to file StarCube_Results.txt*')
+        tracemalloc.stop()
         
-
-        case '2': #BUC
-
-            data = processData('data/data.txt') 
-            start = time.perf_counter()
-            tracemalloc.start()
-            buc = BUC([card for i in range(numDims)], numDims, minsup)
-            buc.buc(data, 0, ['*' for i in range(numDims)])
-            end = time.perf_counter()
-            time = end - start
-            toContinue = input('Algorithm finshed output list to txt? y/n: ')
-            if(toContinue == 'y'):
-                buc_Results = buc.getResults()
-                f = open('results/BUC_Results.txt', 'w')
-                f.write('BUC time: ' + time + '\n')
-                f.write('BUC memory usage: '+ tracemalloc.get_traced_memory()+ '\n')
-                f.write('The average CPU usage is: '+ convert_to_percent(load_tuple))
-                f.write('List: \n' + buc_Results)
-                print('*List printed to file BUC_Results.txt*')
-            tracemalloc.stop()
-
-        case '3': #Star-Cube
-            
-            data = processData('data/data.txt') 
-            start = time.perf_counter()
-            tracemalloc.start()
-            star = starCube(data, MINSUP)
-            star.generateCube()
-            end = time.perf_counter()
-            time = end - start
-            toContinue = input('Algorithm finshed output list to txt? y/n: ')
-            if(toContinue == 'y'):
-                starCube_results = star.getResults()
-                f = open('results/StarCube_Results.txt', 'w')
-                f.write('Star-Cube time: ' + time + '\n')
-                f.write('Star-Cube memory usage: '+ tracemalloc.get_traced_memory()+ '\n')
-                f.write('The average CPU usage is: '+ convert_to_percent(load_tuple))
-                f.write('List: \n' + starCube_results)
-                print('*List printed to file StarCube_Results.txt*')
-            tracemalloc.stop()
-            
-        case '4': #TDC
-            data = processData('data/data.txt') 
-            start = time.perf_counter()
-            tracemalloc.start()
-            tdc = TDC([card for i in range(numDims)], numDims, minsup)
-            tdc.tdc(data, [i for i in range(numDims)])
-            end = time.perf_counter()
-            time = end - start
-            toContinue = input('Algorithm finshed output list to txt? y/n: ')
-            if(toContinue == 'y'):
-                tdc_results = tdc.getResults()
-                f = open('results/TDC_Results.txt', 'w')
-                f.write('TDC time: ' + time + '\n')
-                f.write('TDC memory usage: '+ tracemalloc.get_traced_memory()+ '\n')
-                f.write('The average CPU usage is: '+ convert_to_percent(load_tuple))
-                f.write('List: \n' + tdc_results)
-                print('*List printed to file TDC_Results.txt*')
- 
-
-
-
+        
+        minsup *= 10
