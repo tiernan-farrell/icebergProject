8c72c4df3508e83a4486a77332e68db5b9614dfa
tiernan-farrell <63930496+tiernan-farrell@users.noreply.github.com>
2022-11-28 19:59:24 -0500

    Inserted new test data into graphs (#41)
    Co-authored-by: tiernan-farrell <farrell.331@osu.edu>

--- react-frontend/src/Pages/HundredTuples.js
+++ react-frontend/src/Pages/HundredTuples.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import Graph from '../Components/Graph'
 import ResultsNav from '../Components/ResultsNav'
-import { testOneTimeData, testOneMemoryData, testTwoMemoryData, testTwoTimeData, testOneTime, testOneMemory, testTwoTime, testTwoMemory } from '../graphData'
+import { testOneTimeData, testOneMemoryData, testTwoMemoryData, testTwoTimeData, testOneTime, 
+    testOneMemory, testTwoTime, testTwoMemory, testThreeMemory, testThreeMemoryData, testThreeTime, testThreeTimeData,
+    testFourMemory, testFourTime, testFourMemoryData, testFourTimeData
+} from '../graphData'
 
 
 const HundredTuples = () => {
@@ -12,6 +15,10 @@ const HundredTuples = () => {
         <Graph data={testOneMemoryData} title={testOneMemory}/>
         <Graph data={testTwoTimeData} title={testTwoTime}/>
         <Graph data={testTwoMemoryData} title={testTwoMemory}/>  
+        <Graph data={testThreeTimeData} title={testThreeTime}/>
+        <Graph data={testThreeMemoryData} title={testThreeMemory}/>
+        <Graph data={testFourTimeData} title={testFourTime}/>
+        <Graph data={testFourMemoryData} title={testFourMemory}/>
         </>    
     )
 }
--- react-frontend/src/Pages/ThousandTuples.js
+++ react-frontend/src/Pages/ThousandTuples.js
@@ -1,24 +1,25 @@
 import React from 'react'
 import Graph from '../Components/Graph'
 import ResultsNav from '../Components/ResultsNav'
-
-import { testFiveMemoryData, testFiveTimeData, testSixTimeData, testSixMemoryData, testSevenTimeData, testSevenMemoryData,testFiveTime, testFiveMemory, testSixTime, testSixMemory, testSevenTime, testSevenMemory } from '../graphData'
-
+import { 
+    testNineMemory, testNineMemoryData, testNineTime, testNineTimeData,
+    testTenMemory, testTenMemoryData, testTenTime, testTenTimeData,
+    testElevenMemory, testElevenMemoryData, testElevenTime, testElevenTimeData,
+    testTwelveMemory, testTwelveMemoryData, testTwelveTime, testTwelveTimeData,    
+ } from '../graphData'
 
 const ThousandTuples = (() => {
     return (
         <>
             <ResultsNav />
-            <Graph data={testFiveTimeData} title={testFiveTime}/>
-            <Graph data={testFiveMemoryData} title={testFiveMemory}/>
-
-
-            <Graph data={testSixTimeData} title={testSixTime}/>
-            <Graph data={testSixMemoryData} title={testSixMemory}/>
-
-
-            <Graph data={testSevenTimeData} title={testSevenTime}/>
-            <Graph data={testSevenMemoryData} title={testSevenMemory}/>
+            <Graph data={testNineTimeData} title={testNineTime}/>
+            <Graph data={testNineMemoryData} title={testNineMemory}/>
+            <Graph data={testTenTimeData} title={testTenTime}/>
+            <Graph data={testTenMemoryData} title={testTenMemory}/>  
+            <Graph data={testElevenTimeData} title={testElevenTime}/>
+            <Graph data={testElevenMemoryData} title={testElevenMemory}/>
+            <Graph data={testTwelveTimeData} title={testTwelveTime}/>
+            <Graph data={testTwelveMemoryData} title={testTwelveMemory}/>
 
         </>)
 })
